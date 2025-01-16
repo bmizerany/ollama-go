ef206e24fef2a6effedfd4d5dd4a75145b998ada
Blake Mizerany <blake.mizerany@gmail.com>
2025-01-15 20:59:59 -0800

    client/ollama: add docs to Trace.DownloadUpdate

--- client/ollama/trace.go
+++ client/ollama/trace.go
@@ -3,5 +3,11 @@ package ollama
 import "github.com/bmizerany/ollama-go/blob"
 
 type Trace struct {
-	DownloadUpdate func(d blob.Digest, n, size int64) error
+	// DownloadUpdate periodically reports the progress of a blob download.
+	// The digest d is being downloaded, n bytes have been downloaded so
+	// far, and the total size of the blob is size bytes.
+	//
+	// If an error occurred during the download, d, n, and size will be
+	// their current values, and err will be non-nil.
+	DownloadUpdate func(d blob.Digest, n, size int64, err error)
 }
