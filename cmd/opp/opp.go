57ccab3712774477e514567e059a500c8f15a658
Blake Mizerany <blake.mizerany@gmail.com>
2025-01-17 08:59:17 -0800

    cmd/opp: add model argument to pull from registry

--- cmd/opp/opp.go
+++ cmd/opp/opp.go
@@ -2,20 +2,31 @@ package main
 
 import (
 	"context"
+	"flag"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/bmizerany/ollama-go/client/ollama"
 )
 
 func main() {
+	flag.Parse()
+
 	var rc ollama.Registry
 	c, err := ollama.DefaultCache()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	model := flag.Arg(0)
+	if model == "" {
+		fmt.Fprintln(os.Stderr, "Usage: opp <model>")
+		os.Exit(1)
+	}
+
 	ctx := context.Background()
-	if err := rc.Pull(ctx, c, "library/llama3.2:latest"); err != nil {
+	if err := rc.Pull(ctx, c, model); err != nil {
 		log.Fatal(err)
 	}
 }
