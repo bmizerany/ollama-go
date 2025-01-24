0db61965880e6fbe1c3032aab5d19b033bdab2b0
Blake Mizerany <blake.mizerany@gmail.com>
2025-01-23 22:25:39 -0800

    ...

--- internal/names/name.go
+++ internal/names/name.go
@@ -8,13 +8,13 @@ import (
 const MaxNameLength = 50 + 1 + 50 + 1 + 50 // <namespace>/<model>:<tag>
 
 type Name struct {
-	host string
-	n    string
-	m    string
-	t    string
+	h string
+	n string
+	m string
+	t string
 }
 
-func (n Name) Host() string      { return n.host }
+func (n Name) Host() string      { return n.h }
 func (n Name) Namespace() string { return n.n }
 func (n Name) Model() string     { return n.m }
 func (n Name) Tag() string       { return n.t }
@@ -32,12 +32,13 @@ func parseName(s string) Name {
 		switch c {
 		case ':':
 			n.t = tail
+			continue // look for model
 		case '/':
-			n.n = s
+			n.h, n.n, _ = cutLastAny(s, "/")
 			n.m = tail
 			return n
 		case 0:
-			n.m = s
+			n.m = tail
 			return n
 		}
 	}
@@ -60,19 +61,23 @@ func (n Name) String() string {
 }
 
 func (n Name) GoString() string {
-	return fmt.Sprintf("<Name %q %q %q %q>", n.host, n.n, n.m, n.t)
+	return fmt.Sprintf("<Name %q %q %q %q>", n.h, n.n, n.m, n.t)
 }
 
 // IsFullyQualified returns true if the name is fully qualified, i.e. it has
 // a host, Namespace, Model, and Tag.
 func (n Name) IsValid() bool {
-	return n.host != "" && n.n != "" && n.m != "" && n.t != ""
+	return n.h != "" && n.n != "" && n.m != "" && n.t != ""
 }
 
+// cutLastAny is like strings.Cut but scans in reverse for the last character
+// in chars. If no character is found, before is the empty string and after is
+// s. The returned sep is the byte value of the character in chars that was
+// found; otherwise 0.
 func cutLastAny(s, chars string) (before, after string, sep byte) {
 	i := strings.LastIndexAny(s, chars)
 	if i >= 0 {
 		return s[:i], s[i+1:], s[i]
 	}
-	return s, "", 0
+	return "", s, 0
 }
