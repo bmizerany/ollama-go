dc9a74da500834f6cbafab2433ba169ced23fac7
Blake Mizerany <blake.mizerany@gmail.com>
2025-01-23 22:00:27 -0800

    ...

--- internal/names/name.go
+++ internal/names/name.go
@@ -1,24 +1,24 @@
 package names
 
 import (
+	"fmt"
 	"strings"
 )
 
 const MaxNameLength = 50 + 1 + 50 + 1 + 50 // <namespace>/<model>:<tag>
 
 type Name struct {
-	host      string
-	namespace string
-	model     string
-	tag       string
+	host string
+	n    string
+	m    string
+	t    string
 }
 
 func (n Name) Host() string      { return n.host }
-func (n Name) Namespace() string { return n.namespace }
-func (n Name) Model() string     { return n.model }
-func (n Name) Tag() string       { return n.tag }
+func (n Name) Namespace() string { return n.n }
+func (n Name) Model() string     { return n.m }
+func (n Name) Tag() string       { return n.t }
 
-// h/n/m:t
 func parseName(s string) Name {
 	if len(s) > MaxNameLength {
 		return Name{}
@@ -31,14 +31,13 @@ func parseName(s string) Name {
 		s, tail, c = cutLastAny(s, "/:")
 		switch c {
 		case ':':
-			n.tag = tail
+			n.t = tail
 		case '/':
-			n.namespace = s
-			n.model = tail
-			n.host, _, _ = cutLastAny(s, "/")
+			n.n = s
+			n.m = tail
 			return n
 		case 0:
-			n.model = s
+			n.m = s
 			return n
 		}
 	}
@@ -48,28 +47,32 @@ func parseName(s string) Name {
 // <namespace>/<model>:<tag>.
 func (n Name) String() string {
 	var b strings.Builder
-	if n.namespace != "" {
-		b.WriteString(n.namespace)
+	if n.n != "" {
+		b.WriteString(n.n)
 		b.WriteByte('/')
 	}
-	b.WriteString(n.model)
-	if n.tag != "" {
+	b.WriteString(n.m)
+	if n.t != "" {
 		b.WriteByte(':')
-		b.WriteString(n.tag)
+		b.WriteString(n.t)
 	}
 	return b.String()
+}
+
+func (n Name) GoString() string {
+	return fmt.Sprintf("<Name %q %q %q %q>", n.host, n.n, n.m, n.t)
 }
 
 // IsFullyQualified returns true if the name is fully qualified, i.e. it has
 // a host, Namespace, Model, and Tag.
 func (n Name) IsValid() bool {
-	return n.host != "" && n.namespace != "" && n.model != "" && n.tag != ""
+	return n.host != "" && n.n != "" && n.m != "" && n.t != ""
 }
 
-func cutLastAny(s, seps string) (before, after string, sep byte) {
-	i := strings.LastIndexAny(s, seps)
-	if i < 0 {
-		return s, "", 0
+func cutLastAny(s, chars string) (before, after string, sep byte) {
+	i := strings.LastIndexAny(s, chars)
+	if i >= 0 {
+		return s[:i], s[i+1:], s[i]
 	}
-	return s[:i], s[i+1:], s[i]
+	return s, "", 0
 }
